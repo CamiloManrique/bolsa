332e68d77c8ee5edd2dc5567c35241faac3ea979
Camilo Manrique <camilomanriquec@gmail.com>
2017-02-23 14:49:21 -0500

    Cambios en La14Service

---
+++ src/services/fakebackend.ts
@@ -0,0 +1,11 @@
+import {Injectable} from "../../node_modules/@angular/core/src/di/metadata";
+/**
+ * Created by Administrador on 23/02/2017.
+ */
+
+export const Online: boolean = true;
+export const Users: Object[] = [
+    {user: "Camilo", documento: "1.144.054.421", puntos: "20"},
+    {user: "Juan David", documento: "1.057.478.211", puntos: "50"},
+    {user: "Jorge", documento: "1.871.214.781", puntos: "10"},
+];
--- src/services/la14api.service.ts
+++ src/services/la14api.service.ts
@@ -3,29 +3,56 @@
  */
 import { Injectable } from '@angular/core';
 import {Http, Response} from "@angular/http";
+import {Online, Users} from "./fakebackend";
 
-
+//Modificar por las variables reales
+export var BASE_URL = "http://jsonplaceholder.typicode.com";
+export var USER_ENDPOINT = "/posts/1";
 
 @Injectable()
 export class La14Service{
-
-    constructor(private http: Http){ }
-
+    constructor(
+        private http: Http
+    ){  }
 
     getUserData(user: string): Promise<any>{
+
+        // Fake Request (for testing purposes)
+
+        if(!Online){
+            return Promise.reject("El servicio no está disponible");
+        }
+
+        for(let object of Users){
+            if(user == user) {
+                return Promise.resolve(object);
+            }
+        }
+        return Promise.resolve({});
+
+
+
+
+
+        // Real Http Request
+        /*
         return new Promise((resolve, reject) => {
-            this.http.request("http://jsonplaceholder.typicode.com/posts/1")
+            this.http.request(`${BASE_URL}${USER_ENDPOINT}`)
             .subscribe((res: Response) => {
                 resolve(res.json());
-            })
+            },
+            error => reject(null))
         })
+        */
     }
 
 
     getUserPoints(user: string): Promise<string> {
         return new Promise((resolve, reject) => {
             this.getUserData(user).then((userdata: any) => {
-                resolve(userdata.body);
+                resolve(userdata.body); //Reemplazar por el miembro apropiado de puntos
+            }).catch((error: any) => {
+                reject(error);
             })
         })
     }
