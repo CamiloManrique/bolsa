6075e2718bd92496b518fb6116c0358443776038
Juan David Marcillo Alba <jdm.a0125@gmail.com>
2017-03-14 16:10:58 -0500

    Toast Controller

---
+++ src/providers/service-partners.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the ServicePartners provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class ServicePartners {
+
+  data:any
+
+  constructor(public http: Http) {
+    //console.log('Hello ServicePartners Provider');
+    this.data = null;
+  }
+
+  selectPartners(page:any){
+
+    this.data = null;
+
+    return new Promise((resolve, reject) =>{
+
+
+      let myHeader = new Headers({
+        "Content-Type":"text/html; charset=UTF-8"
+      });
+
+      let options = new RequestOptions({
+          headers: myHeader
+      });
+
+      this.http.get('http://localhost/aplicacion/index.php/Partners/getAll/'+page,options)
+      .map(res => res.json())
+      .subscribe(data => {
+
+          this.data = data;
+          resolve(this.data);
+
+      },
+      error => {reject(error)}
+      );
+
+    });
+
+
+
+  }
+
+
+
+
+
+
+}
